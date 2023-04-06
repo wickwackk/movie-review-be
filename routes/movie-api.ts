46375a601fb4bbdf6cde942529571f2e4a1a60b5
Morning <>
2023-04-06 12:14:12 +0800

    apr 6

--- routes/movie-api.ts
+++ routes/movie-api.ts
@@ -1,15 +1,22 @@
 import express, { Express, Request, Response } from "express";
-import { getMovies } from "../services/movie-service";
+import { getMovies, getSingleMovie } from "../services/movie-service";
 // import { getProducts } from "../services/product-service.js";
 
 const movie_router = express.Router();
 
 // const upload = multer({ dest: "/tmp" });
 
-movie_router.get("/movie", async (req: Request, res: Response) => {
+movie_router.get("/movies", async (req: Request, res: Response) => {
   console.log("request to get products");
   const movies = await getMovies(10);
   res.status(200).json(movies);
+});
+
+movie_router.get("/movie/:id", async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  const reqId = req.params.id;
+  const movie = await getSingleMovie(1, reqId);
+  res.status(200).json(movie);
 });
 
 // movie_router.post("/product", upload.single("file"), async (req, res) => {
--- services/movie-service.ts
+++ services/movie-service.ts
@@ -4,11 +4,23 @@ import Movie from "../model/Movie";
 export async function getMovies(limit: number) {
   return await Movie.find({ poster: { $exists: true } })
     .limit(limit)
+    .select({ title: 1, _id: 1, poster: 1, tomatoes: 1 })
     .then((movies) => {
       return movies;
     })
     .catch((err) => {
       console.log("Error getting movies", err);
+    });
+}
+
+export async function getSingleMovie(limit: number, reqId: string) {
+  return await Movie.findOne({ _id: reqId })
+    .limit(limit)
+    .then((movie) => {
+      return movie;
+    })
+    .catch((err) => {
+      console.log("Error getting a single movie", err);
     });
 }
 
